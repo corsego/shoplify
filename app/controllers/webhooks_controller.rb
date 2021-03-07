e874c279e60df36662da691f965a545e6289c8ec
Ubuntu <ubuntu@ip-172-31-15-185.eu-central-1.compute.internal>
2021-03-07 21:12:12 +0000

    add stripe webhooks

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,33 @@
+class WebhooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def create
+    payload = request.body.read
+    sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+    event = nil
+
+    begin
+      event = Stripe::Webhook.construct_event(
+        payload, sig_header, Rails.application.credentials[:stripe][:webhook]
+      )
+    rescue JSON::ParserError => e
+      status 400
+      return
+    rescue Stripe::SignatureVerificationError => e
+      # Invalid signature
+      puts "Signature error"
+      p e
+      return
+    end
+
+    # Handle the event
+    case event.type
+    when 'checkout.session.completed'
+      session = event.data.object
+      @product = Product.find_by(price: session.amount_total)
+      @product.increment!(:sales_count)
+    end
+
+    render json: { message: 'success' }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "products#index"
   resources :products
   post "checkout/create", to: "checkout#create"
+  resources :webhooks, only: [:create]
 end
