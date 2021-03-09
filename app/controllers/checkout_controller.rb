487acbfbf395e9c51727ca9542cab5cc07c8f1f9
Yaro <yshmarov@gmail.com>
2021-03-09 18:27:57 +0000

    success and cancel paths for stripe checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -10,12 +10,19 @@ class CheckoutController < ApplicationController
         quantity: 1
       }],
       mode: 'payment',
-      success_url: root_url + "?session_id={CHECKOUT_SESSION_ID}",
-      cancel_url: root_url,
+      success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: cancel_url,
     })
     respond_to do |format|
       format.js
     end
   end
+
+  def success
+  end
+
+  def cancel
+  end
+
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :products
   post "checkout/create", to: "checkout#create"
   resources :webhooks, only: [:create]
+  get "success", to: "checkout#success"
+  get "cancel", to: "checkout#cancel"
 end
