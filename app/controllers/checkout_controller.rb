a094810ff35e8a9f14843fe5dbad2ec93a6b6d50
Yaro <yshmarov@gmail.com>
2021-03-11 12:48:48 +0000

    allow_promotion_codes for checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -5,6 +5,7 @@ class CheckoutController < ApplicationController
       customer: current_user.stripe_customer_id,
       payment_method_types: ['card'],
       line_items: @cart.collect { |item| item.to_builder.attributes! },
+      allow_promotion_codes: true,
       mode: 'payment',
       success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
       cancel_url: cancel_url,
