a43e7c88661becede21d76eb2f4af400cf8d64f9
Yaro <yshmarov@gmail.com>
2021-03-11 12:01:42 +0000

    if product price is updated - create new stripe price and assing to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -19,4 +19,10 @@ class Product < ApplicationRecord
     update(stripe_product_id: product.id, stripe_price_id: price.id)
   end
 
+  after_update :create_and_assign_new_stripe_price, if: :saved_change_to_price?
+  def create_and_assign_new_stripe_price
+    price = Stripe::Price.create(product: self.stripe_product_id, unit_amount: self.price, currency: "usd")
+    update(stripe_price_id: price.id)
+  end
+
 end
