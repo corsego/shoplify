aab01a13bf77241d4e0b372302b730b00cd31220
Yaro <yshmarov@gmail.com>
2021-03-12 11:27:08 +0000

    products with different currencies can not be selected in one cart

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,17 +1,21 @@
 class CheckoutController < ApplicationController
   
   def create
-    @session = Stripe::Checkout::Session.create({
-      customer: current_user.stripe_customer_id,
-      payment_method_types: ['card'],
-      line_items: @cart.collect { |item| item.to_builder.attributes! },
-      allow_promotion_codes: true,
-      mode: 'payment',
-      success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
-      cancel_url: cancel_url,
-    })
-    respond_to do |format|
-      format.js
+    if @cart.pluck(:currency).length > 1
+      redirect_to products_path, alert: "You can not select products with different currencies in one checkout"
+    else
+      @session = Stripe::Checkout::Session.create({
+        customer: current_user.stripe_customer_id,
+        payment_method_types: ['card'],
+        line_items: @cart.collect { |item| item.to_builder.attributes! },
+        allow_promotion_codes: true,
+        mode: 'payment',
+        success_url: success_url + "?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url: cancel_url,
+      })
+      respond_to do |format|
+        format.js
+      end
     end
   end
 
