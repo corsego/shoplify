117f9712e8aea1bf7d353c78511c08d2b32dba5f
Yaro <yshmarov@gmail.com>
2021-03-12 11:27:30 +0000

    no card error if a product was deleted from database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,11 @@ class ApplicationController < ActionController::Base
   end
 
   def load_cart
-    @cart = Product.find(session[:cart])
+    # @cart = Product.find(session[:cart])
+    # Product.all.pluck(:id) => 1, 2
+    # Product.find(1, 2, 3) => error
+    # Product.where(id: [1, 2, 3]) => 1, 2
+    @cart = Product.where(id: session[:cart])
   end
 
 end
