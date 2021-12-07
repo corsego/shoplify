2dd89fd1b3847738f0208a3f212f172bbaeae044
Yaro <yshmarov@gmail.com>
2021-12-07 18:11:47 +0000

    config hosts nil

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.hosts << "ad3f0369707e49e0a641afda0bd0d7ba.vfs.cloud9.eu-central-1.amazonaws.com"
+  config.hosts = nil
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
